38c96f3b59362934e0cf6dcdd6674e71fc77c172
Kun Zhang <feel@shaw.ca>
2022-10-15 23:15:24 -0600

    Added Account Controller for user authentication

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -6,8 +6,10 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace API.Controllers
@@ -15,13 +17,14 @@ namespace API.Controllers
     [Route("[controller]")]
     public class AccountController : Controller
     {
-        private readonly DataContext _Context;
+        private readonly DataContext _context;
         
-        public AccountController(DataContext Context)    {
-            _Context = Context;
+        public AccountController(DataContext context)    {
+            _context = context;
         }
 
         [HttpPost("register")]
+        
         public async Task<ActionResult<AppUser>> Register(string username, string password){
             using var hmac = new HMACSHA512();
             var user = new AppUser{
@@ -30,8 +33,20 @@ namespace API.Controllers
                 PasswordSalt = hmac.Key
             };
 
-            _Context.Users.Add(user);
-            await _Context.SaveChangesAsync();
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x=>x.UserName==loginDto.Username);
+            if(user == null) return Unauthorized("Invalid username");
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var ComputeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+            for(int i = 0; i<ComputeHash[i])
+                return Unauthorized("Invalid Password");
             return user;
         }
     }
