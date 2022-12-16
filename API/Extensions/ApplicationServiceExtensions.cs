c86b1abaf7e0411bd8125bb0b575cb908499c9e0
Kun Zhang <feel@shaw.ca>
2022-12-15 22:35:34 -0700

    update lesson 123-126 adding photo upload

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -32,6 +32,9 @@ namespace API.Extensions
             services.AddDbContext<DataContext>(options =>{
                 options.UseSqlite(config.GetConnectionString("DefaultConnection"));
             });
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
+            services.AddScoped<IPhotoService, PhotoService>();
+            
             return services;
         }
     }
